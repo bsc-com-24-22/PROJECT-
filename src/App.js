28d3e72f8f226408ca80b560b3e7528ee60865bd
bsc-com-24-22 <bsc-com-24-22@unima.ac.mw>
2024-05-22 21:06:15 +0200

    transferring from one app to another

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import "./index.css";
+import Header from "./Components/Header/Header"
+import Aboutus from './pages/AboutUs/AboutUs';
+import Footer from './components/Footer/Footer';
+import NavBbar from './Components/navBbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />cd
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='flex flex-col'>
+      <div  className = "flex flex-col w-full ">
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' elements={<Aboutus />} />
+
+    </Routes>
+    
+    </BrowserRouter>
+        </div>
+        </div>
   );
 }
 

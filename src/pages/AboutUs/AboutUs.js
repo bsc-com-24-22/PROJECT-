ae664f5e4053fb36b2216032ab92ba28c397612f
bsc-com-24-22 <bsc-com-24-22@unima.ac.mw>
2024-05-23 13:59:27 +0200

    Photo sizes

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -27,7 +27,7 @@ export default function AboutusPage() {
             
 
             <div className='text-xl'>
-                <h1 className='font-extrabold sm:text-5xl text-customebrown'>ABOUT US</h1>
+                <h1 className='font-extrabold sm:text-5xl text-custombrown'>ABOUT US</h1>
                 <p className=" font-sans  font-bold first-letter:text-bold first-letter:text-2xl sm:text-preety">Welcome to delivery express!</p>
                 <p className=" font-sans  font-bold first-letter:only: sm:text-preety">Established by seven innovative students From unima</p>
                 <p className='font-sans  font-bold first-letter:only: sm:text-preety'>From unima we do express deriveries</p>
@@ -35,14 +35,14 @@ export default function AboutusPage() {
             <img src={img6} alt="" className='  w-z h-96 '></img>
         </div>
       
-        <h2 className='flex justify-center font-extrabold sm:text-5xl?'>MEET OUR TEAM</h2>
+        <h2 className='flex justify-center font-bold sm:text-5xl'>MEET OUR TEAM</h2>
         <div className='flex gap-2 w-screen justify-evenly '>
             <img src={img1} alt=" "className='w-1/4'></img>
             <img src={img2} alt='' className='w-1/4'></img>
             <img src={img3} alt=""className=' w-1/4'></img>
 
         </div>
-        <h3 className='flex justify-center font-extrabold sm:text-5xl?'>CUSTOMER FEEDBACK</h3>
+        <h3 className='flex justify-center font-bold sm:text-5xl'>CUSTOMER FEEDBACK</h3>
         <div className='flex  justify-center mb-6'>
             <img src={img8} alt="" className=' w-1/4 z-0'></img>
         </div>
